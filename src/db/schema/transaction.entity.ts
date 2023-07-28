ad45130459b69bcb5c21ed5c54910821a3ac3c2b
andresmarpz <andresmarpz@gmail.com>
2023-07-27 23:14:52 -0300

    create transaction entity schema

---
+++ src/db/schema/transaction.entity.ts
@@ -0,0 +1,26 @@
+import { numeric, pgTable, text } from "drizzle-orm/pg-core"
+import { InferModel, relations } from "drizzle-orm"
+import { baseEntity } from "@/db/schema/base.entity"
+import { accounts } from "@/db/schema/account.entity"
+
+export const transactions = pgTable("transactions", {
+  ...baseEntity,
+  description: text("description"),
+  amount: numeric("amount", { precision: 24, scale: 2 }),
+  fee: numeric("fee", { precision: 24, scale: 2 }),
+  fromAccountId: baseEntity.id,
+  toAccountId: baseEntity.id,
+})
+
+export const transactionsRelations = relations(transactions, ({ one }) => ({
+  fromAccount: one(accounts, {
+    fields: [transactions.fromAccountId],
+    references: [accounts.id],
+  }),
+  toAccount: one(accounts, {
+    fields: [transactions.toAccountId],
+    references: [accounts.id],
+  }),
+}))
+
+export type Transaction = InferModel<typeof transactions>
