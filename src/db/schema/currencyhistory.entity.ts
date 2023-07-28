0213c1e51326be749c9ad44027fdabca3dc049d3
andresmarpz <andresmarpz@gmail.com>
2023-07-27 23:15:28 -0300

    create currencyhistory entity schema

---
+++ src/db/schema/currencyhistory.entity.ts
@@ -0,0 +1,22 @@
+import { InferModel, relations } from "drizzle-orm"
+import { pgTable } from "drizzle-orm/pg-core"
+import { baseEntity } from "@/db/schema/base.entity"
+import { currencies } from "@/db/schema/currency.entity"
+
+export const currencyHistories = pgTable("currency_histories", {
+  ...baseEntity,
+  currencyId: baseEntity.id,
+  value: baseEntity.id,
+})
+
+export const currencyHistoriesRelations = relations(
+  currencyHistories,
+  ({ one }) => ({
+    currency: one(currencies, {
+      fields: [currencyHistories.currencyId],
+      references: [currencies.id],
+    }),
+  })
+)
+
+export type CurrencyHistory = InferModel<typeof currencyHistories>
