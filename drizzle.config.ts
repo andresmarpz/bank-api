b1959be52187926e8b2d4f56cbe452d947921e8c
andresmarpz <andresmarpz@gmail.com>
2023-10-19 15:36:01 -0300

    migrate to Bun and fix docker-compose

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -10,7 +10,7 @@ export default {
   out: "drizzle",
   driver: "pg",
   dbCredentials: {
-    host: process.env.POSTGRES_HOST ?? "localhost",
+    host: "127.0.0.1",
     port: Number(process.env.POSTGRES_PORT),
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
